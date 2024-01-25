baf0544962489afdd9531c3c75a770cc579f4d15
yp945 <pan107104@outlook.com>
2024-01-25 15:15:20 +0800

    add decode function output example.

--- examples/book_decode.rs
+++ examples/book_decode.rs
@@ -10,10 +10,18 @@ fn main() {
         serde_json::from_reader(file).expect("failed to parse ABI")
     };
 
-    let data = vec![60, 5, 111, 108, 97, 118, 109, 7, 120553111];
+    let input_data = vec![60, 5, 111, 108, 97, 118, 109, 7, 120553111];
 
-    // Decode
-    let (func, decoded_data) = abi.decode_input_from_slice(&data).unwrap();
+    // Decode input 
+    let (func, decoded_data) = abi.decode_input_from_slice(&input_data).unwrap();
 
-    println!("decode function {:?}\n data {:?}", func.name, decoded_data);
+    println!("decode function input {:?}\n data {:?}", func.name, decoded_data);
+
+    // Decode output "hello"
+    let output_data = vec![5, 104, 101, 108, 108, 111, 6];
+
+    let decoded_data = abi.decode_output_from_slice(abi.functions[1].signature().as_str(), &output_data).unwrap();
+
+    println!("decode function output {:?}\n", decoded_data);
+    
 }
