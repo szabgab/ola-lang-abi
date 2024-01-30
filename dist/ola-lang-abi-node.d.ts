c39db0203b4505824a001ac289222cee653da18e
bchyl <bchylmn@gmail.com>
2024-01-30 16:34:30 +0800

    updata package

---
+++ dist/ola-lang-abi-node.d.ts
@@ -0,0 +1,35 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Uint8Array} file_content
+* @param {BigUint64Array} data
+* @returns {any}
+*/
+export function decode_abi_wrapper(file_content: Uint8Array, data: BigUint64Array): any;
+/**
+* @param {Uint8Array} file_content
+* @param {BigUint64Array} data
+* @returns {any}
+*/
+export function decode_input_from_js(file_content: Uint8Array, data: BigUint64Array): any;
+/**
+* @param {Uint8Array} file_content
+* @param {string} signature
+* @param {BigUint64Array} data
+* @returns {any}
+*/
+export function decode_output_wrapper(file_content: Uint8Array, signature: string, data: BigUint64Array): any;
+/**
+* @param {Uint8Array} file_content
+* @param {string} signature
+* @param {BigUint64Array} data
+* @returns {any}
+*/
+export function decode_output_from_js(file_content: Uint8Array, signature: string, data: BigUint64Array): any;
+/**
+* @param {Uint8Array} file_content
+* @param {string} signature
+* @param {any} params
+* @returns {any[]}
+*/
+export function encode_input_from_js(file_content: Uint8Array, signature: string, params: any): any[];
